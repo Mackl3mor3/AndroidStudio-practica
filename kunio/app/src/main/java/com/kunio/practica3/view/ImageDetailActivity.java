88602de8ca455bd85c76b5137edd865b8315d786
Julio Cesar M <41308581+Mackl3mor3@users.noreply.github.com>
2020-12-11 23:40:00 -0400

    Practica acabada

---
+++ kunio/app/src/main/java/com/kunio/practica3/view/ImageDetailActivity.java
@@ -0,0 +1,26 @@
+package com.kunio.practica3.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+import com.kunio.practica3.R;
+
+public class ImageDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_detail);
+
+        showToolBar("", true);
+    }
+    public void showToolBar(String titulo, boolean botonSubir) {
+        Toolbar toolbar = findViewById(R.id.toolbar_image_detail);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+}
